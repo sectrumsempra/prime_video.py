0b9f30ef8b378b56f6a9b8edfacb17c684b2c4cd
sectrumsempra <53516516+sectrumsempra@users.noreply.github.com>
2020-10-25 20:15:12 +0530

    Update Sel_Beaut.py

--- Sel_Beaut.py
+++ Sel_Beaut.py
@@ -10,8 +10,6 @@ movie_descriptions = []
 movie_ratings = []
 def open_site():
     options = webdriver.ChromeOptions()
-    options.add_argument("--user-data-dir=C:/Users/sectr/AppData/Local/Google/Chrome/User Data/Default")
-    options.add_argument("--profile-directory=Default")
     options.add_argument("--disable-notifiactions")
     driver = webdriver.Chrome(executable_path='C:/Users/sectr/OneDrive/Desktop/PythonProjects/chromedriver_win32/chromedriver.exe',options=options)
     driver.get(r'https://www.amazon.com/ap/signin?accountStatusPolicy=P1&clientContext=261-1149697-3210253&language=en_US&openid.assoc_handle=amzn_prime_video_desktop_us&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.primevideo.com%2Fauth%2Freturn%2Fref%3Dav_auth_ap%3F_encoding%3DUTF8%26location%3D%252Fref%253Ddv_auth_ret')
